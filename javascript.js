0f930c5ca381ab14351eaf34abd8b98a23ec37f1
Warren <55094820+warrenreilly@users.noreply.github.com>
2020-02-09 01:56:29 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function changeTheme() {
+
+
+    var body = document.getElementById("body");
+    var headTxt = document.getElementById("head");
+
+body.className = "body2";
+headTxt.className = "h12";
+
+
+}
+
+function changeBack() {
+
+
+    var body = document.getElementById("body");
+    var headTxt = document.getElementById("head");
+
+body.className = "body1";
+headTxt.className = "h1";
+
+
+}
+
+
+// enterhtml java
